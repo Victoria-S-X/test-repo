a7a1e80a27312cb5603a8511bda0ee40bc91719d
shiyaox <shiyaox@student.chalmers.se>
2023-08-30 19:43:33 +0200

    first commit

---
+++ module.js
@@ -0,0 +1,8 @@
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./alternatives')
+require('./nums')
+
+console.log(data)
+sayHi(names.john)
+sayHi(names.peter)
